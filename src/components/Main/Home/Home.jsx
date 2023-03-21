4abb881ed9e31ab4bbd6bd703febb1630cea19ae
ZeberMVP <rubeen722@gmail.com>
2023-03-21 12:55:58 +0100

    edit/home chat gtt

--- src/components/Main/Home/Home.jsx
+++ src/components/Main/Home/Home.jsx
@@ -31,15 +31,15 @@ import logo25 from '../../../assets/logo25.svg';
 export default function Home() {
   return (
     <main>
-      <section className="w-[1368px] bg-orange3 rounded-[32px] h-[617px] mt-36 mx-6 mb-20">
-        <img src={group1} alt="Gente" className="mt-[-12px] ml-[520px] absolute" />
+      <section className="w-[1368px] bg-orange3 rounded-[32px] h-[640px] mt-36 mx-6 mb-20">
+        <img src={group1} alt="Gente" className="mt-[5.84px] ml-[480px] absolute" />
         <form action="/">
-          <input type="text" className="w-[417px] h-[115px] ml-[120px] mt-[430px] shadow-md rounded absolute" />
-          <img src={send} alt="Enviar" className='absolute ml-[509px] mt-[520px]' />
+          <input type="text" className="w-[373px] h-[137px] ml-[80px] mt-[430px] shadow-md rounded absolute" />
+          <img src={send} alt="Enviar" className='absolute ml-[419px] mt-[520px]' />
         </form>
-        <p className='absolute text-[21.36px] text-green font-medium font-["Roboto"] w-[509px] ml-[120px] mt-[192px] leading-[30.36px]'>Sabemos que recibir un diagnóstico de VIH puede ser abrumador y puede haber muchas preguntas que surjan. Pero no estás solx. Estamos aquí para ayudarte y brindarte información veraz y útil.</p>
-        <p className='absolute text-[22px] text-green font-bold w-[270px] ml-[120px] mt-[340px]'>Pregunta lo que quieras:</p>
-        <p className='absolute text-[57px] text-orange font-bold ml-[120px] mt-16 leading-[64px]'>Chat gTt</p>
+        <p className='absolute text-[21.36px] text-green font-medium font-["Roboto"] w-[469px] ml-[80px] mt-[163px] leading-[30.36px]'>Sabemos que recibir un diagnóstico de VIH puede ser abrumador y puede haber muchas preguntas que surjan. Pero no estás solx. Estamos aquí para ayudarte y brindarte información veraz y útil.</p>
+        <p className='absolute text-[21.36px] text-green font-bold w-[270px] ml-[80px] mt-[355px]'>Pregunta lo que quieras:</p>
+        <p className='absolute text-[62.69px] font-["Poppins"] text-orange font-bold ml-[80px] mt-[72px] leading-[64px]'>Chat gTt</p>
       </section>
       <section className='w-[1128px] h-[605px] my-20 ml-[156px]'>
         <p className='font-["Poppins"] text-green font-bold text-4xl'>Últimas noticias</p>
