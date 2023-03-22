87882d1e51e856870db3b5fefd123c64ef45074e
ZManak <javiefe.dev@gmail.com>
2023-03-22 18:22:46 +0100

    view/article

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from './Home/Home';
 import About from './About/About';
-
+import Article from './Article/Article';
 
 export default function Main() {
     return (
         <Routes>
             <Route element={<Home />} path="/" />
             <Route element={<About />} path="/sobre-el-vih" />
+            <Route element={<Article />} path="/articulo" />
         </Routes>
     )
 }
